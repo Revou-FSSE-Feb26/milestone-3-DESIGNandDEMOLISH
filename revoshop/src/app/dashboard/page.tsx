bdab96d4ab16c5d1f1d2363682e8e2a3eb8b35ab
alexander <alexander.chua.ac@gmail.com>
2026-06-05 20:46:28 +0700

    add dashboard

---
+++ revoshop/src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+import Navigation from "@/components/NavigationBar"
+
+export default function DashboardDetail() {
+    return (
+        <div>
+            <Navigation />
+            <h1>Dashboard</h1>
+        </div>
+    )
+}
